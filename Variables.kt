060acffbb318344a097b12460de97d9b330bd457
Matt Doren <mattdoren62@gmail.com>
2020-02-03 11:40:55 -0600

    Add files via upload

---
+++ Variables.kt
@@ -0,0 +1,37 @@
+
+fun main(){
+    // Numbers
+    var myInt: Int = 10
+    var myLong = 40L
+    var myFloat: Float = 3.14F
+    var myDouble: Double = 3.14159265
+
+    var anotherInt = 12
+
+    // Booleans
+    var myBoolean: Boolean = true
+    myBoolean = false
+
+    // Strings
+    var myString1: String = "This is a string."
+    var myString2 = "This string has a newline \n"
+    var multiLines = """
+        This is line 1
+        This is line 2
+        This is line 3
+        """
+
+    val acctBalance = 200
+    val myBal = "My balance is $acctBalance"
+
+    fun myCode(){
+        var myByte: Byte = 0
+        var myShort: Short = 1
+        var myInt: Int = 32
+        var myLong: Long = 64
+        var myFloat: Float = 3.14159265F
+        var myDouble: Double = 64.1
+        var myBoolean: Boolean = true
+        var myChar: Char = 'f'
+    }
+}
